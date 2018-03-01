d138321c4d779195311310f45ebc698db5b9480c
mgamatero <mgamatero@yahoo.com>
2018-03-01 11:00:16 -0800

    still images ok now.  .on.click,id,function used.  gifs * buttons now show

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,16 +6,17 @@ $(document).ready(function () {
 
     //function to dynamically add buttons.  Parameters 
     function dynamicButtonAdd(buttonName, divToAppendTo) {
-        var a = $("<button>")
-        a.addClass("btn btn-primary")
+        var a = $('<button>')
+        a.addClass('btn btn-primary')
         a.text(buttonName)
         buttonNumber++
-        a.attr("index", buttonNumber)
+        a.attr('index', buttonNumber)
         $(divToAppendTo).append(a)
     }
 
     //onclick event that adds dynamic buttons.  Calls function dyamicButtonAdd
-    $("#addTopic").on("click", function () {
+    // $("#addTopic").on("click", function () {
+    $(document).on("click", "#addTopic", function () {
         event.preventDefault()
         topicInput = $("#addInput").val()
         dynamicButtonAdd(topicInput, "#topicButtons")
@@ -23,7 +24,7 @@ $(document).ready(function () {
         $("#topicResult").empty()
 
         //onclick event that searches giphy api when buttons clicked.  Buttons text are used to search
-        $(".btn-primary").on("click", function (document) {
+        $(document).on('click', '.btn-primary', function () {
             $("#topicResult").empty()
             var APIKEY = "4aW0ucG0qcPg0ONPsekdiLuJCcUFRAzX"
             var searchMe = $(this).text()
@@ -35,24 +36,25 @@ $(document).ready(function () {
             }).then(function (requestResult) {
                 console.log('getResponse', requestResult.data)
                 for (var i = 0; i < requestResult.data.length; i++) {
-                    $("#topicResult").append(i + " " + "<img src='" + requestResult.data[i].images.fixed_height.url + " 'index="+i+"><br>")
+                    $("#topicResult").append(i + " " + "<img src='" + requestResult.data[i].images.fixed_height.url + " 'index=" + i + "><br>")
                     $("img").attr("state", "active")
                     $("#topicResult").append("<p>Rating: " + requestResult.data[i].rating + "</p>")
                 }
                 //onclick event that pauses and restarts gifs
-                $("img").on("click", function () {
+                // $("img").on("click", function () {
+                $(document).on("click", "img", function () {
                     var state = $(this).attr("state")
                     if (state === "active") {
                         //debug - alert("current state is" + $(this).attr("state"))
                         $(this).attr("src", requestResult.data[$(this).attr("index")].images.fixed_height_still.url)
                         $(this).attr("state", "still")
-                       //debug - alert("state is now" + $(this).attr("state"))
+                        //debug - alert("state is now" + $(this).attr("state"))
                     }
                     else {
                         //debug - alert("current state is" + $(this).attr("state"))
                         $(this).attr("src", requestResult.data[$(this).attr("index")].images.fixed_height.url)
                         $(this).attr("state", "active")
-                       //debug - alert("state is now" + $(this).attr("state"))
+                        //debug - alert("state is now" + $(this).attr("state"))
                     }
                 })
             })//end ajax
